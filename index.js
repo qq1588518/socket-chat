9c4bebaf195aa28e3bce36a63b76a23c159b5d70
clayeo <ocatuu@gmail.com>
2018-02-04 17:04:38 +0800

    rewrite index.js

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var jade = require('jade');
 
 
 app.use(express.static('src/assets/'));
 
 app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/src/index.html')
+    var html = jade.renderFile('src/index.jade');
+    res.send(html);
 })
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
     })
-})
+});
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
