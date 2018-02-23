3ddcaebe3331ed5b7e4e020e81660e07af74bd3f
Clayeo <ocatuu@gmail.com>
2018-02-23 14:27:17 +0800

    index.js

--- index.js
+++ index.js
@@ -4,19 +4,41 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var jade = require('jade');
 
-
 app.use(express.static('src/assets/'));
 
 app.get('/', (req,res) => {
     var html = jade.renderFile('src/index.jade');
     res.send(html);
-})
+});
+
+var users = [];
 
 io.on('connection', (socket) => {
+    // 监听发送消息
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
-    })
+    });
+    // 监听添加用户
+    socket.on('addUser', (msg) => {
+        for(let i =0; i< users.length; i++){
+            if(users[i].username === msg.username){
+                io.emit('addUser error',users);
+                return false;
+            }
+        }
+        users.push({
+            username: msg.username,
+            useravatar: msg.useravatar
+        });
+        io.emit('addUser success',users);
+    });
+    // 监听在线用户
+    socket.on('show online', (msg) => {
+        io.emit('show online',users);
+    });
 });
+
+
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
